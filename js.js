9f2a6d6eb861ed00cffecaf765374e6b6a076aaa
Damian Sopel <dsopel0@gmail.com>
2017-11-26 22:12:17 -0600

    added carousel slideshow

---
+++ js.js
@@ -0,0 +1,64 @@
+$(function(event) {
+	$('.hamburger-icon').click(function(event) {
+		$(".menu").slideToggle("slow");
+		console.log("clicked")
+	});
+});
+
+var start = null;
+
+$('.next').click(function() {
+	var currentSlide = $('.slide.active');
+	var nextSlide = currentSlide.next();
+
+	currentSlide.fadeOut(300).removeClass('active');
+	nextSlide.fadeIn(300).addClass('active');
+
+	if(nextSlide.length ==0) {
+		$('.slide').first().fadeIn(300).addClass('active')
+	}
+	pauseSlides();
+});
+
+$('.prev').click(function() {
+	var currentSlide = $('.slide.active');
+	var prevSlide = currentSlide.prev();
+
+	currentSlide.fadeOut(300).removeClass('active');
+	prevSlide.fadeIn(300).addClass('active');
+
+	if(prevSlide.length == 0) {
+		$('.slide').last().fadeIn(300).addClass('active')
+	}
+	pauseSlides();
+})
+
+function changeSlides() {
+	var currentSlide = $('.slide.active');
+	var nextSlide = currentSlide.next();
+	currentSlide.fadeOut(300).removeClass('active');
+	nextSlide.fadeIn(300).addClass('active');
+
+	if(nextSlide.length ==0) {
+		$('.slide').first().fadeIn(300).addClass('active')
+	}
+}
+
+function startSlides() {
+	if (!start) {
+		start = setInterval(changeSlides, 6000)
+	}
+}
+
+function pauseSlides() {
+	if (start) {
+		clearInterval(start);
+		start = null;
+
+		setTimeout(startSlides, 3000)
+	}
+}
+
+$(document).ready(function() {
+	startSlides();
+});
